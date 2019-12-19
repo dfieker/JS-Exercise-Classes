ee90ffa30e4cc9eae7ac7a9a65ad15aba13c83ac
Daniel Fieker <danielmfieker@gmail.com>
2019-12-19 15:06:17 -0600

    First Commit

--- index.js
+++ index.js
@@ -41,7 +41,22 @@ class Airplane {
 */
 
 class Person {
-
+  constructor (name, age) {
+    this.name = name,
+    this.age = age,
+    this.stomach = []
+  }
+  eat(food) {
+    if(this.stomach.length < 10) {
+      this.stomach.push(food);
+    }
+  }
+  poop() {
+    this.stomach.length = 0;
+  }
+  toString() {
+    return `${this.name}, ${this.age}`;
+  }
 }
 
 /*
@@ -59,7 +74,18 @@ class Person {
 */
 
 class Car {
-
+  constructor (model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank += gallons;
+  }
+  drive(distance) {
+    this.odometer
+  }
 }
 
 /*
